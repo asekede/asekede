2454eb7d6aa5f3c0f4d74eee7d900990b6d06147
asekede <assylkhan.abdrakhmanov@gmail.com>
2018-03-11 12:06:38 +0600

    added templates folder

--- src/asekede/urls.py
+++ src/asekede/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls)
 ]
