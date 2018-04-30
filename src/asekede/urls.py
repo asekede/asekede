91d075b7e61e4afe7a7b316c4d263221697336e5
asekede <assylkhan.abdrakhmanov@gmail.com>
2018-04-30 17:33:47 +0600

    modified structure, added about me page(not finished)

--- src/asekede/urls.py
+++ src/asekede/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='about_me.html'), name='about_me'),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls)
 ]
--- src/blog/models.py
+++ src/blog/models.py
@@ -101,8 +101,6 @@ class Post(models.Model):
     def get_list_of_latest_posts_by_category(name):
         category = Category.get_category_with_name(name)
         posts = Post.get_list_of_latest_posts().filter(category=category)
-        if len(posts) == 0:
-            raise Http404("No posts by given category")
         return posts
 
     @staticmethod
@@ -116,8 +114,6 @@ class Post(models.Model):
                 ) 
             )
         ).filter(exists=True)
-        if len(posts) == 0:
-            raise Http404("No posts by given tag")
         return posts
 
     @staticmethod
