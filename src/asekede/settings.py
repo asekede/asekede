b0a3539788ca3202df3a0e4c4abb297144805436
asekede <assylkhan.abdrakhmanov@gmail.com>
2018-06-06 00:17:03 +0600

    added google analytics support

--- src/asekede/settings.py
+++ src/asekede/settings.py
@@ -12,14 +12,15 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
+INSTALLED_APPS = [ 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'google_analytics',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- src/asekede/urls.py
+++ src/asekede/urls.py
@@ -5,5 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='about_me.html'), name='about_me'),
     path('blog/', include('blog.urls')),
+    path('djga/', include('google_analytics.urls')),
     path('admin/', admin.site.urls)
 ]
